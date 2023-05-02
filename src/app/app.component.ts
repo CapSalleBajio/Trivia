6eac1c34456605734d8389930cc18fa12747df2d
Josué Caro <josuecaro@MacBook-Pro-de-Josue.local>
2023-05-02 16:09:33 -0600

    Final de Clase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FirabaseService } from './services/firabase.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'trivia';
+
+  constructor(
+    private firabaseService: FirabaseService,
+  ) {
+    this.getData().then();
+  }
+
+  async import() {
+    console.log("Importing data...");
+    await this.firabaseService.importData();
+    console.log("Data imported!");
+  }
+
+  async getData() {
+    const questions = await this.firabaseService.getAllQuestions();
+    console.log(questions);
+  }
+
 }
